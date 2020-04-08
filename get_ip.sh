2881cca3fc967d13f8d87b7092f75d83f9b125aa
Jie Liu <liujie333333@gmail.com>
2020-04-08 15:11:29 +0800

    Add log

--- get_ip.sh
+++ get_ip.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 ip_old=ip_old.txt
 ip_now=ip_now.txt
+log=log
 
 ## init ip.old
 
 while [ ! -f $ip_old ]; do
-curl http://members.3322.org/dyndns/getip  > $ip_old
+        curl http://members.3322.org/dyndns/getip  > $ip_old
 done
 ## get ip now
 
@@ -14,6 +15,7 @@ curl http://members.3322.org/dyndns/getip > $ip_now
 /usr/bin/diff $ip_now $ip_old
 
 if [ $? != 0 ];then
-cat $ip_now > $ip_old
-echo "IP has changed,new ip is$(cat $ip_now) !!!" | curl "https://sc.ftqq.com/SCUKEY.send?text=$(cat $ip_now)"
+        sed -i "\$a$(date) ------ IP change detected, previous ip: $(cat $ip_old), new ip: $(cat $ip_now)." $log
+        cat $ip_now > $ip_old
+        curl "https://sc.ftqq.com/SCUKEY.send?text=$(cat $ip_now)";
 fi
